6091b077c5a420f6a98117ce8e5cd161ffd8782b
valentrukus <valenrivas2@gmail.com>
2025-08-28 13:24:20 -0300

    aaa

--- Assets/Resources/_Scripts/Player/ChangeWeapon.cs
+++ Assets/Resources/_Scripts/Player/ChangeWeapon.cs
@@ -1,3 +1,4 @@
+using Photon.Pun;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -12,6 +13,12 @@ public class ChangeWeapon : MonoBehaviour
 
     private int currentWeaponIndex = 0;
     private Weapon currentWeapon;
+    private PhotonView photonView;
+
+    private void Awake()
+    {
+        photonView = GetComponent<PhotonView>();
+    }
 
     private void Start()
     {
@@ -21,23 +28,24 @@ public class ChangeWeapon : MonoBehaviour
 
     private void Update()
     {
+        if (!photonView.IsMine) return;
         if (Input.GetKeyUp(KeyCode.Q))
         {
             NextWeapon();
         }
     }
 
-    private void EquipWeapon(int index)
-    {
-        for (int i = 0; i < weapons.Count; i++)
-            weapons[i].SetActive(i == index);
+    //private void EquipWeapon(int index)
+    //{
+    //    for (int i = 0; i < weapons.Count; i++)
+    //        weapons[i].SetActive(i == index);
 
-        currentWeaponIndex = index;
-        currentWeapon = weapons[index].GetComponent<Weapon>();
+    //    currentWeaponIndex = index;
+    //    currentWeapon = weapons[index].GetComponent<Weapon>();
 
-        if (currentWeapon != null && crosshairUI != null)
-            crosshairUI.sprite = currentWeapon.crosshair;
-    }
+    //    if (currentWeapon != null && crosshairUI != null)
+    //        crosshairUI.sprite = currentWeapon.crosshair;
+    //}
 
     private void NextWeapon()
     {
@@ -47,5 +55,23 @@ public class ChangeWeapon : MonoBehaviour
             nextIndex = 0;
 
         EquipWeapon(nextIndex);
+    }
+
+    [PunRPC]
+    void EquipWeaponRPC(int weaponIndex)
+    {
+        for (int i = 0; i < weapons.Count; i++)
+            weapons[i].SetActive(i == weaponIndex);
+
+        currentWeaponIndex = weaponIndex;
+        currentWeapon = weapons[weaponIndex].GetComponent<Weapon>();
+
+        if (photonView.IsMine && crosshairUI != null)
+            crosshairUI.sprite = currentWeapon.crosshair;
+    }
+
+    private void EquipWeapon(int index)
+    {
+        photonView.RPC("EquipWeaponRPC", RpcTarget.AllBuffered, index);
     }
 }
