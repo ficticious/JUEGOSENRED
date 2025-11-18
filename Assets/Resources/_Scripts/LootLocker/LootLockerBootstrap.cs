198b1a2aaf15e31be6dfa5a27d53aac2e316102f
mmlago <mmlago0@gmail.com>
2025-11-18 15:34:20 -0300

    Active el guest

--- Assets/Resources/_Scripts/LootLocker/LootLockerBootstrap.cs
+++ Assets/Resources/_Scripts/LootLocker/LootLockerBootstrap.cs
@@ -1,4 +1,4 @@
-using LootLocker.Requests;
+﻿using LootLocker.Requests;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,7 +7,7 @@ public class LootLockerBootstrap : MonoBehaviour
 {
     public static bool SessionStarted { get; private set; }
 
-    [SerializeField] string playerIdentifier = "1";
+    string playerIdentifier = System.Guid.NewGuid().ToString();
 
     private void Awake()
     {
@@ -17,15 +17,16 @@ public class LootLockerBootstrap : MonoBehaviour
 
     void StartGuest()
     {
-        LootLockerSDKManager.StartGuestSession(playerIdentifier, response =>
+        LootLockerSDKManager.StartGuestSession((response) =>
         {
             if (!response.success)
             {
-                Debug.LogError("Fallo");
+                Debug.LogError($"FALLO al iniciar sesión → code: {response.statusCode}, error: {response.errorData?.message}");
                 return;
             }
+
             SessionStarted = true;
-            Debug.Log("Conectado");
+            Debug.Log("Conectado correctamente a LootLocker");
         });
     }
 }
