2ca55617e0f8aca759a08400b257724ce7df87ee
ficticious <magustinmeli@gmail.com>
2025-11-18 17:04:27 -0300

    Chat de texto

---
+++ Assets/Resources/_Scripts/Message.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+public class Message : MonoBehaviour
+{
+    public TextMeshProUGUI MyMessage;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
