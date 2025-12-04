bd82b17d7d03a3a9aac3eb66ea50e5de82438195
mmlago <mmlago0@gmail.com>
2025-12-04 13:30:23 -0300

    Salto Arreglado

--- Assets/Resources/_Scripts/Player/Movement.cs
+++ Assets/Resources/_Scripts/Player/Movement.cs
@@ -50,11 +50,14 @@ public class Movement : MonoBehaviour
     {
         if (!photonView.IsMine) return;
 
+        HandleMovement();
+        HandleJump();
+
         input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
         input.Normalize();
 
         sprinting = Input.GetButton("Sprint");
-        jumping = Input.GetButton("Jump");
+        jumping = Input.GetButtonDown("Jump");
 
         if (groundCheck != null)
             grounded = Physics.CheckSphere(groundCheck.position, groundCheckRadius, groundMask);
@@ -69,8 +72,7 @@ public class Movement : MonoBehaviour
     {
         if (!photonView.IsMine) return;
 
-        HandleMovement();
-        HandleJump();
+        
 
         anim.SetBool("Moving", input.magnitude > 0.1f);
     }
